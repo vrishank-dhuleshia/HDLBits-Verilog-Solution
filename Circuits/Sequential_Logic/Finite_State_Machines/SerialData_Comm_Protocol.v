ef6c62d9d99c63d34062200099b3e18bf404b14b
Vrishank <coldlemon2007@gmail.com>
2026-07-16 13:22:48 +0530

    Solved the Serial Data Protocol Using Count Method.

--- Circuits/Sequential_Logic/Finite_State_Machines/SerialData_Comm_Protocol.v
+++ Circuits/Sequential_Logic/Finite_State_Machines/SerialData_Comm_Protocol.v
@@ -2,6 +2,57 @@ module top_module(
     input clk,
     input in,
     input reset,    // Synchronous reset
+    output [7:0] out_byte,
+    output done
+);
+    parameter [2:0] START=3'd0, COUNTING=3'd1, IN_CHECK=3'd2, DONE=3'd3, WAIT=3'd4;
+    reg [2:0] state, next_state;
+    
+    reg [3:0] count;
+    reg [7:0] out_byte_reg;
+    
+    always @(*) begin
+        case(state)
+            START : next_state = in ? START : COUNTING;
+            COUNTING : next_state = (count==4'd8) ? IN_CHECK : COUNTING;
+            IN_CHECK : next_state = in ? DONE : WAIT;
+            DONE : next_state = in ? START : COUNTING;
+            WAIT : next_state = in ? START : WAIT;
+            default : next_state = START;
+        endcase
+    end
+    
+    always @(posedge clk) begin
+        if(reset) begin
+            state<=START;
+            count<=4'd0;
+            out_byte_reg<=8'd0;
+        end
+        else begin
+            state<=next_state;
+            if(state==COUNTING) begin
+                out_byte_reg[count-1]<=in;
+            end
+            if(next_state==COUNTING) begin
+                count<=count+4'd1;
+            end
+            else begin
+                count<=4'd0;
+            end
+        end
+    end
+    
+    assign done = (state==DONE);
+    assign out_byte = (state==DONE) ? out_byte_reg : 8'b0;
+            
+endmodule
+
+//OLD CODE WITH MORE FLIP FLOPS
+/*
+module top_module(
+    input clk,
+    input in,
+    input reset,    // Synchronous reset
     output reg [7:0] out_byte,
     output done
 ); //
