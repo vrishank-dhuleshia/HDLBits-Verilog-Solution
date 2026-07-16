cc627b03c54d4190b1f9e4616ac645911e72511e
Vrishank <coldlemon2007@gmail.com>
2026-07-16 16:46:56 +0530

    Derived the expression for Y0 and Z by using KMap from the truth table given.

---
+++ Circuits/Sequential_Logic/Finite_State_Machines/26_Q3c.v
@@ -0,0 +1,12 @@
+module top_module (
+    input clk,
+    input [2:0] y,
+    input x,
+    output Y0,
+    output z
+);
+    
+    assign Y0 = y[0] ^ x ^ (y[2] & ~y[1] & ~y[0]);
+    assign z  = (y[1] & y[0]) | (y[2] & ~y[1] & ~y[0]);
+
+endmodule
