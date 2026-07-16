55907d5faa55754b5e26387de1f78cc399402966
Vrishank <coldlemon2007@gmail.com>
2026-07-16 12:44:11 +0530

    Tried different and better approach to solve the problem.

--- Circuits/Sequential_Logic/Finite_State_Machines/Serial_Comm_Protocol.v
+++ Circuits/Sequential_Logic/Finite_State_Machines/Serial_Comm_Protocol.v
@@ -4,6 +4,52 @@ module top_module(
     input reset,    // Synchronous reset
     output done
 ); 
+
+    parameter [2:0] START=3'd0, COUNTING=3'd1, IN_CHECK=3'd2, DONE=3'd3, WAIT=3'd4;
+    reg [2:0] state, next_state;
+    
+    reg [3:0] count;
+    
+    always @(*) begin
+        case(state)
+            START : next_state = in ? START : COUNTING;
+            COUNTING : next_state = (count==4'd8) ? IN_CHECK : COUNTING;
+            IN_CHECK : next_state = in ? DONE : WAIT;
+            DONE : next_state = in ? START : COUNTING;
+            WAIT : next_state = in ? START : WAIT;
+            default : next_state = START;
+        endcase
+    end
+    
+    always @(posedge clk) begin
+        if(reset) begin
+            state<=START;
+            count<=4'd0;
+        end
+        else begin
+            state<=next_state;
+            if(next_state==COUNTING) begin
+                count<=count+4'd1;
+            end
+            else begin
+                count<=4'd0;
+            end
+        end
+    end
+    
+    assign done = (state==DONE);
+            
+endmodule
+
+//OLD CODE 
+//USED MORE FLIP FLOPS
+
+/* module top_module(
+    input clk,
+    input in,
+    input reset,    // Synchronous reset
+    output done
+); 
     
     parameter [3:0] SB=4'b0,
     B1=4'b0001, B2=4'b0010,
@@ -43,3 +89,4 @@ module top_module(
 
     assign done = (state==SB && prev_state==Dock_stage && prev2_state==B8);
 endmodule
+*/ 
